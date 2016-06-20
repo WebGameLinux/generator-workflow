4cbdbf85225e55799365ec999dde5bf0670bd6a0
littledu <littledu@tencent.com>
2016-06-20 21:44:53 +0800

    1.3.2

--- app/templates/_tasks/TaskBuildDist.js
+++ app/templates/_tasks/TaskBuildDist.js
@@ -24,260 +24,260 @@ var revDel = require('gulp-rev-delete-original');
 var changed = require('./common/changed')();
 
 var paths = {
-  src: {
-    dir: './src',
-    img: './src/img/**/*.{JPG,jpg,png,gif}',
-    slice: './src/slice/**/*.png',
-    js: './src/js/**/*.js',
-    media: './src/media/**/*',
-    less: './src/css/style-*.less',
-    lessAll: './src/css/**/*.less',
-    html: ['./src/html/**/*.html', '!./src/html/_*/**.html'],
-    htmlAll: './src/html/**/*',
-    php: './src/**/*.php'
-  },
-  tmp: {
-    dir: './tmp',
-    css: './tmp/css',
-    img: './tmp/img',
-    html: './tmp/html',
-    sprite: './tmp/sprite'
-  },
-  dist: {
-    dir: './dist',
-    css: './dist/css',
-    img: './dist/img',
-    html: './dist/html',
-    sprite: './dist/sprite'
-  }
+    src: {
+        dir: './src',
+        img: './src/img/**/*.{JPG,jpg,png,gif}',
+        slice: './src/slice/**/*.png',
+        js: './src/js/**/*.js',
+        media: './src/media/**/*',
+        less: './src/css/style-*.less',
+        lessAll: './src/css/**/*.less',
+        html: ['./src/html/**/*.html', '!./src/html/_*/**.html'],
+        htmlAll: './src/html/**/*',
+        php: './src/**/*.php'
+    },
+    tmp: {
+        dir: './tmp',
+        css: './tmp/css',
+        img: './tmp/img',
+        html: './tmp/html',
+        sprite: './tmp/sprite'
+    },
+    dist: {
+        dir: './dist',
+        css: './dist/css',
+        img: './dist/img',
+        html: './dist/html',
+        sprite: './dist/sprite'
+    }
 };
 
 module.exports = function (gulp, config) {
-  var webp = require('./common/webp')(config);
+    var webp = require('./common/webp')(config);
 
-  var lazyDir = config.lazyDir || ['../slice'];
+    var lazyDir = config.lazyDir || ['../slice'];
 
-  var postcssOption = [];
+    var postcssOption = [];
 
-  if (config.supportREM) {
-    postcssOption = [
-      postcssAutoprefixer({browsers: ['last 5 versions']}),
-      postcssPxtorem({
-        root_value: '20', // 基准值 html{ font-zise: 20px; }
-        prop_white_list: [], // 对所有 px 值生效
-        minPixelValue: 2 // 忽略 1px 值
-      })
-    ]
-  } else {
-    postcssOption = [
-      postcssAutoprefixer({browsers: ['last 5 versions']})
-    ]
-  }
-
-  // 清除 dist 目录
-  function delDist() {
-    return del([paths.dist.dir]);
-  }
-
-  // 清除 tmp 目录
-  function delTmp() {
-    return del([paths.tmp.dir]);
-  }
-
-  //编译 less
-  function compileLess() {
-    return gulp.src(paths.src.less)
-      .pipe(less())
-      .pipe(lazyImageCSS({imagePath: lazyDir}))
-      .pipe(tmtsprite({margin: 4}))
-      .pipe(gulpif('*.png', gulp.dest(paths.tmp.sprite), gulp.dest(paths.tmp.css)));
-  }
-
-  //自动补全
-  function compileAutoprefixer() {
-    return gulp.src('./tmp/css/style-*.css')
-      .pipe(postcss(postcssOption))
-      .pipe(gulp.dest('./tmp/css/'));
-  }
-
-  //CSS 压缩
-  function miniCSS() {
-    return gulp.src('./tmp/css/style-*.css')
-      .pipe(minifyCSS({
-        safe: true,
-        reduceTransforms: false,
-        advanced: false,
-        compatibility: 'ie7',
-        keepSpecialComments: 0
-      }))
-      .pipe(gulp.dest('./tmp/css/'));
-  }
-
-  //图片压缩
-  function imageminImg() {
-    return gulp.src(paths.src.img)
-      .pipe(imagemin({
-        use: [pngquant()]
-      }))
-      .pipe(gulp.dest(paths.tmp.img));
-  }
-
-  //复制媒体文件
-  function copyMedia() {
-    return gulp.src(paths.src.media, {base: paths.src.dir}).pipe(gulp.dest(paths.dist.dir));
-  }
-
-  //JS 压缩
-  function uglifyJs() {
-    return gulp.src(paths.src.js, {base: paths.src.dir})
-      .pipe(uglify())
-      .pipe(gulp.dest(paths.tmp.dir));
-  }
-
-  //雪碧图压缩
-  function imageminSprite() {
-    return gulp.src('./tmp/sprite/**/*')
-      .pipe(imagemin({
-        use: [pngquant()]
-      }))
-      .pipe(gulp.dest(paths.tmp.sprite));
-  }
-
-  //html 编译
-  function compileHtml() {
-    return gulp.src(paths.src.html)
-      .pipe(ejs(ejshelper()))
-      .pipe(gulpif(
-        config.supportREM,
-        posthtml(
-          posthtmlPx2rem({
-            rootValue: 20,
-            minPixelValue: 2
-          })
-        ))
-      )
-      .pipe(usemin({  //JS 合并压缩
-        jsmin: uglify()
-      }))
-      .pipe(gulp.dest(paths.tmp.html));
-  }
-
-  //webp 编译
-  function supportWebp() {
-    if (config['supportWebp']) {
-      return webp();
+    if (config.supportREM) {
+        postcssOption = [
+            postcssAutoprefixer({browsers: ['last 5 versions']}),
+            postcssPxtorem({
+                root_value: '20', // 基准值 html{ font-zise: 20px; }
+                prop_white_list: [], // 对所有 px 值生效
+                minPixelValue: 2 // 忽略 1px 值
+            })
+        ]
     } else {
-      return function noWebp(cb) {
-        cb();
-      }
+        postcssOption = [
+            postcssAutoprefixer({browsers: ['last 5 versions']})
+        ]
     }
-  }
 
-  //新文件名(md5)
-  function reversion(cb) {
-    var revAll = new RevAll({
-      fileNameManifest: 'manifest.json',
-      dontRenameFile: ['.html', '.php']
-    });
-
-    if (config['reversion']) {
-      return gulp.src(['./tmp/**/*'])
-        .pipe(revAll.revision())
-        .pipe(gulp.dest(paths.tmp.dir))
-        .pipe(revDel({
-          exclude: /(.html|.htm)$/
-        }))
-        .pipe(revAll.manifestFile())
-        .pipe(gulp.dest(paths.tmp.dir));
-    } else {
-      cb();
+    // 清除 dist 目录
+    function delDist() {
+        return del([paths.dist.dir]);
     }
-  }
 
-  function findChanged(cb) {
+    // 清除 tmp 目录
+    function delTmp() {
+        return del([paths.tmp.dir]);
+    }
 
-    if (!config['supportChanged']) {
-      return gulp.src('./tmp/**/*', {base: paths.tmp.dir})
-        .pipe(gulp.dest(paths.dist.dir))
-        .on('end', function () {
-          delTmp();
-        })
-    } else {
-      var diff = changed('./tmp');
-      var tmpSrc = [];
+    //编译 less
+    function compileLess() {
+        return gulp.src(paths.src.less)
+            .pipe(less({relativeUrls: true}))
+            .pipe(lazyImageCSS({imagePath: lazyDir}))
+            .pipe(tmtsprite({margin: 4}))
+            .pipe(gulpif('*.png', gulp.dest(paths.tmp.sprite), gulp.dest(paths.tmp.css)));
+    }
 
-      if (!_.isEmpty(diff)) {
+    //自动补全
+    function compileAutoprefixer() {
+        return gulp.src('./tmp/css/style-*.css')
+            .pipe(postcss(postcssOption))
+            .pipe(gulp.dest('./tmp/css/'));
+    }
 
-        //如果有reversion
-        if (config['reversion'] && config['reversion']['available']) {
-          var keys = _.keys(diff);
+    //CSS 压缩
+    function miniCSS() {
+        return gulp.src('./tmp/css/style-*.css')
+            .pipe(minifyCSS({
+                safe: true,
+                reduceTransforms: false,
+                advanced: false,
+                compatibility: 'ie7',
+                keepSpecialComments: 0
+            }))
+            .pipe(gulp.dest('./tmp/css/'));
+    }
 
-          //先取得 reversion 生成的manifest.json
-          var reversionManifest = require(path.resolve('./tmp/manifest.json'));
+    //图片压缩
+    function imageminImg() {
+        return gulp.src(paths.src.img)
+            .pipe(imagemin({
+                use: [pngquant()]
+            }))
+            .pipe(gulp.dest(paths.tmp.img));
+    }
 
-          if (reversionManifest) {
-            reversionManifest = _.invert(reversionManifest);
+    //复制媒体文件
+    function copyMedia() {
+        return gulp.src(paths.src.media, {base: paths.src.dir}).pipe(gulp.dest(paths.dist.dir));
+    }
 
-            reversionManifest = _.pick(reversionManifest, keys);
+    //JS 压缩
+    function uglifyJs() {
+        return gulp.src(paths.src.js, {base: paths.src.dir})
+            .pipe(uglify())
+            .pipe(gulp.dest(paths.tmp.dir));
+    }
 
-            reversionManifest = _.invert(reversionManifest);
+    //雪碧图压缩
+    function imageminSprite() {
+        return gulp.src('./tmp/sprite/**/*')
+            .pipe(imagemin({
+                use: [pngquant()]
+            }))
+            .pipe(gulp.dest(paths.tmp.sprite));
+    }
 
-            _.forEach(reversionManifest, function (item, index) {
-              tmpSrc.push('./tmp/' + item);
-              console.log('[changed:] ' + util.colors.blue(index));
-            });
+    //html 编译
+    function compileHtml() {
+        return gulp.src(paths.src.html)
+            .pipe(ejs(ejshelper()))
+            .pipe(gulpif(
+                config.supportREM,
+                posthtml(
+                    posthtmlPx2rem({
+                        rootValue: 20,
+                        minPixelValue: 2
+                    })
+                ))
+            )
+            .pipe(usemin({  //JS 合并压缩
+                jsmin: uglify()
+            }))
+            .pipe(gulp.dest(paths.tmp.html));
+    }
 
-            //将新的 manifest.json 保存
-            fs.writeFileSync('./tmp/manifest.json', JSON.stringify(reversionManifest));
-
-            tmpSrc.push('./tmp/manifest.json');
-          }
+    //webp 编译
+    function supportWebp() {
+        if (config['supportWebp']) {
+            return webp();
         } else {
-          _.forEach(diff, function (item, index) {
-            tmpSrc.push('./tmp/' + index);
-            console.log('[changed:] ' + util.colors.blue(index));
-          });
+            return function noWebp(cb) {
+                cb();
+            }
+        }
+    }
+
+    //新文件名(md5)
+    function reversion(cb) {
+        var revAll = new RevAll({
+            fileNameManifest: 'manifest.json',
+            dontRenameFile: ['.html', '.php']
+        });
+
+        if (config['reversion']) {
+            return gulp.src(['./tmp/**/*'])
+                .pipe(revAll.revision())
+                .pipe(gulp.dest(paths.tmp.dir))
+                .pipe(revDel({
+                    exclude: /(.html|.htm)$/
+                }))
+                .pipe(revAll.manifestFile())
+                .pipe(gulp.dest(paths.tmp.dir));
+        } else {
+            cb();
+        }
+    }
+
+    function findChanged(cb) {
+
+        if (!config['supportChanged']) {
+            return gulp.src('./tmp/**/*', {base: paths.tmp.dir})
+                .pipe(gulp.dest(paths.dist.dir))
+                .on('end', function () {
+                    delTmp();
+                })
+        } else {
+            var diff = changed('./tmp');
+            var tmpSrc = [];
+
+            if (!_.isEmpty(diff)) {
+
+                //如果有reversion
+                if (config['reversion'] && config['reversion']['available']) {
+                    var keys = _.keys(diff);
+
+                    //先取得 reversion 生成的manifest.json
+                    var reversionManifest = require(path.resolve('./tmp/manifest.json'));
+
+                    if (reversionManifest) {
+                        reversionManifest = _.invert(reversionManifest);
+
+                        reversionManifest = _.pick(reversionManifest, keys);
+
+                        reversionManifest = _.invert(reversionManifest);
+
+                        _.forEach(reversionManifest, function (item, index) {
+                            tmpSrc.push('./tmp/' + item);
+                            console.log('[changed:] ' + util.colors.blue(index));
+                        });
+
+                        //将新的 manifest.json 保存
+                        fs.writeFileSync('./tmp/manifest.json', JSON.stringify(reversionManifest));
+
+                        tmpSrc.push('./tmp/manifest.json');
+                    }
+                } else {
+                    _.forEach(diff, function (item, index) {
+                        tmpSrc.push('./tmp/' + index);
+                        console.log('[changed:] ' + util.colors.blue(index));
+                    });
+                }
+
+                return gulp.src(tmpSrc, {base: paths.tmp.dir})
+                    .pipe(gulp.dest(paths.dist.dir))
+                    .on('end', function () {
+                        delTmp();
+                    })
+
+            } else {
+                console.log('Nothing changed!');
+                delTmp();
+                cb();
+            }
         }
 
-        return gulp.src(tmpSrc, {base: paths.tmp.dir})
-          .pipe(gulp.dest(paths.dist.dir))
-          .on('end', function () {
-            delTmp();
-          })
-
-      } else {
-        console.log('Nothing changed!');
-        delTmp();
-        cb();
-      }
     }
 
-  }
 
+    //加载插件
+    function loadPlugin(cb) {
+        util.loadPlugin('build_dist');
+        cb();
+    }
 
-  //加载插件
-  function loadPlugin(cb) {
-    util.loadPlugin('build_dist');
-    cb();
-  }
-
-  //注册 build_dist 任务
-  gulp.task('build_dist', gulp.series(
-    delDist,
-    compileLess,
-    compileAutoprefixer,
-    miniCSS,
-    gulp.parallel(
-      imageminImg,
-      imageminSprite,
-      copyMedia,
-      uglifyJs
-    ),
-    compileHtml,
-    reversion,
-    supportWebp(),
-    findChanged,
-    loadPlugin
-  ));
+    //注册 build_dist 任务
+    gulp.task('build_dist', gulp.series(
+        delDist,
+        compileLess,
+        compileAutoprefixer,
+        miniCSS,
+        gulp.parallel(
+            imageminImg,
+            imageminSprite,
+            copyMedia,
+            uglifyJs
+        ),
+        compileHtml,
+        reversion,
+        supportWebp(),
+        findChanged,
+        loadPlugin
+    ));
 };
 
